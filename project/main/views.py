6988b52ab8a4a8f68604931e7bd81b9db9f13cb1
ParkSuhhyun <2022111511@dgu.ac.kr>
2024-05-12 01:12:26 +0900

    5주차 과제 + 보너스 1번

--- project/main/views.py
+++ project/main/views.py
@@ -30,7 +30,15 @@ def detail(request, id):  # detail 함수의 매개변수 이름을 id로 수정
         new_comment.pub_date=timezone.now()
 
         new_comment.save()
+
         return redirect('main:detail', id)
+    
+def delete_comment(request, id):
+    comment = get_object_or_404(Comment, pk=id)
+    post_id = comment.post.id
+    if request.user.is_authenticated and request.user == comment.writer:
+        comment.delete()
+    return redirect('main:detail', id=post_id)
 
 def edit(request, id):
     edit_post = Post.objects.get(pk=id)
@@ -84,5 +92,22 @@ def update(request, id):
 
 def delete(request, id):
     delete_post = Post.objects.get(pk=id)
+
+    tags=delete_post.tags.all()
+    for tag in tags:
+        if tag.posts.count()==1:
+            tag.delete()
     delete_post.delete()
     return redirect('main:secondpage')
+
+def tag_list(request):
+    tags=Tag.objects.all()
+    return render(request, 'main/tag-list.html', { 'tags' : tags})
+
+def tag_posts(request, tag_id):
+    tag=get_object_or_404(Tag, id=tag_id)
+    posts=tag.posts.all()
+    return render(request, 'main/tag-post.html', {
+        'tag' : tag,
+        'posts' : posts,
+    })
